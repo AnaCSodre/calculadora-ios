c38c570cce0894bd3bcb2eda53d4f471abd6b2c7
Ana Sodré <ana.sodre@g.globo>
2025-07-11 18:31:03 -0300

    initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ana Sodre on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
